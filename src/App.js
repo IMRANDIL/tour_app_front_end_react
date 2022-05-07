f33326fc794c9f33db53b589abd4545c10b9d3dc
IMRANDIL <aliimranadil2@gmail.com>
2022-05-07 15:19:17 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import './App.css';
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
+import { Link, BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 
 
-function App() {
+
+
+
+const App = () => {
   return (
     <div className="App">
       <ToastContainer />
-      hey ali what is up
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
