3adda9dbf813c14426398321caa65c3db0fe0bef
IMRANDIL <aliimranadil2@gmail.com>
2022-05-07 19:03:35 +0530

    yh

---
+++ src/redux/features/authSlice.js
@@ -0,0 +1,14 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        user: null,
+        error: '',
+        loading: false
+    }
+});
+
+
+export default authSlice.reducer;
