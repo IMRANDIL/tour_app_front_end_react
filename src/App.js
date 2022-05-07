56167ae3ea8b3caecd7f58d22d7a286d1e60ef15
IMRANDIL <aliimranadil2@gmail.com>
2022-05-07 15:11:14 +0530

    yh

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer />
+      hey ali what is up
+    </div>
+  );
+}
+
+export default App;
